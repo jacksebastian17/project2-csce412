29b558c41af0281e4d0a86e0f4071253aa24642e
Jack Sebastian <jacksebastian17@tamu.edu>
2022-09-30 15:05:28 -0500

    added helper function make the request, which includes dummy IP address generator for the request source and dest

--- mainFile.cpp
+++ mainFile.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include "loadbalancer.h"
 #include <time.h>
+#include <ctime>
+#include <cstdlib>
+#include <sstream>
 
 using namespace std;
+
+request createRequest() {
+    stringstream ips, ipd;
+    request r;
+    ips << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256);
+    ipd << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256);
+    r.source = ips.str();
+    r.destination = ipd.str();
+    r.processTime = rand() % 100;
+    return r;
+}
 
 int main() {
     int numWebservers;
@@ -13,15 +27,19 @@ int main() {
     cout << "Please enter the time you want to run the load balancer: ";
     cin >> loadBalancerRuntime;
 
+    srand(time(0));
+
     loadbalancer lb;
     webserver webservers[10];
+
+    for (int i = 0; i < 10; i++) {
+        request newRequest = createRequest();
+        lb.addRequest(newRequest);
+    }
+
     for (int i = 0; i < numWebservers; i++) {
         webserver server((char)('A' + i));
         webservers[i] = server;
-    }
-    for (int i = 0; i < 10; i++) {
-        request newRequest;
-        newRequest.processTime = rand() % 10; // request takes 10 seconds
-        lb.addRequest(newRequest);
+        webservers[i].addRequest(lb.getRequest(), lb.getTime());
     }
 }
